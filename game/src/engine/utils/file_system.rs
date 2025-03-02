6cb6520d0d9ab6fe1a73ba97f924b67c79b059cb
JuH33 <jboyer@ohbibi.com>
2025-03-02 21:29:01 +0100

    fix(fs): better handle of results when getting buffer

--- game/src/engine/utils/file_system.rs
+++ game/src/engine/utils/file_system.rs
@@ -1,6 +1,11 @@
-use std::{env, fs::File, io::{Read, Write}, path::PathBuf};
-use image::io::Reader;
 use crate::engine::rendering::shaders::Texture;
+use image::io::Reader;
+use std::{
+    env,
+    fs::File,
+    io::{Read, Write},
+    path::PathBuf,
+};
 
 static ASSETS_PATH: &str = "assets/";
 static SHADER_PATH: &str = "shaders/";
@@ -13,7 +18,7 @@ pub enum FileType {
     Shader,
     Texture,
     Mesh,
-    Material
+    Material,
 }
 
 pub struct FileSystem;
@@ -25,10 +30,11 @@ impl FileSystem {
         match File::open(&file_path).as_mut() {
             Ok(file) => {
                 let mut file_content: String = String::new();
-                file.read_to_string(&mut file_content).expect(format!("Failed to read file {}", &file_path).as_str());
+                file.read_to_string(&mut file_content)
+                    .expect(format!("Failed to read file {}", &file_path).as_str());
                 Ok(file_content)
             }
-            Err(_) => Err(String::from(format!("Could not open file {}", &file_path)))
+            Err(_) => Err(String::from(format!("Could not open file {}", &file_path))),
         }
     }
 
@@ -37,7 +43,16 @@ impl FileSystem {
 
         println!("[File System] Loading texture: {}", &file_path);
 
-        match Reader::open(&file_path).or(Err("File not Found")).unwrap().decode() {
+        let reader = Reader::open(&file_path);
+
+        if reader.is_err() {
+            return Err(String::from(format!(
+                "[File System] Can't open file {}",
+                file_path
+            )));
+        }
+
+        match reader.unwrap().decode() {
             Ok(img) => {
                 let width = img.width();
                 let height = img.height();
@@ -49,10 +64,10 @@ impl FileSystem {
                     data: img.into_bytes(),
                     width,
                     height,
-                    channels
+                    channels,
                 })
-            },
-            Err(_) => Err(String::from(format!("Could not open file {}", &file_path)))
+            }
+            Err(_) => Err(String::from(format!("Could not open file {}", &file_path))),
         }
     }
 
@@ -60,7 +75,8 @@ impl FileSystem {
         let asset_path: String = FileSystem::get_path(file_path, f_type);
 
         let mut file = File::create(&asset_path).expect("Could not create file");
-        file.write_all(contents.as_bytes()).expect("Could not write to file");
+        file.write_all(contents.as_bytes())
+            .expect("Could not write to file");
     }
 
     fn get_path(file_path: &str, f_type: FileType) -> String {
@@ -69,16 +85,40 @@ impl FileSystem {
 
         match f_type {
             FileType::Shader => {
-                path = current_dir.join(ASSETS_PATH).join(SHADER_PATH).join(file_path).into_os_string().into_string().unwrap();
+                path = current_dir
+                    .join(ASSETS_PATH)
+                    .join(SHADER_PATH)
+                    .join(file_path)
+                    .into_os_string()
+                    .into_string()
+                    .unwrap();
             }
             FileType::Texture => {
-                path = current_dir.join(ASSETS_PATH).join(TEXTURE_PATH).join(file_path).into_os_string().into_string().unwrap();
+                path = current_dir
+                    .join(ASSETS_PATH)
+                    .join(TEXTURE_PATH)
+                    .join(file_path)
+                    .into_os_string()
+                    .into_string()
+                    .unwrap();
             }
             FileType::Mesh => {
-                path = current_dir.join(ASSETS_PATH).join(MESH_PATH).join(file_path).into_os_string().into_string().unwrap();
+                path = current_dir
+                    .join(ASSETS_PATH)
+                    .join(MESH_PATH)
+                    .join(file_path)
+                    .into_os_string()
+                    .into_string()
+                    .unwrap();
             }
             FileType::Material => {
-                path = current_dir.join(ASSETS_PATH).join(MATERIAL_PATH).join(file_path).into_os_string().into_string().unwrap();
+                path = current_dir
+                    .join(ASSETS_PATH)
+                    .join(MATERIAL_PATH)
+                    .join(file_path)
+                    .into_os_string()
+                    .into_string()
+                    .unwrap();
             }
         }
 
